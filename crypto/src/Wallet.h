353a4118aac3c7ce90d43104b03ceaebac4b84f4
hwangs12 <sungjun94@gmail.com>
2023-09-11 21:42:29 -0400

    add header

---
+++ crypto/src/Wallet.h
@@ -0,0 +1,19 @@
+#include <string>
+#include <map>
+
+class Wallet
+{
+public:
+    Wallet();
+
+    /* insert currency to the wallet */
+    void insertCurrency(std::string type, double amount);
+
+    /* check if the wallet contains this much currency or more */
+    bool containsCurrency(std::string type, double amount);
+
+    /* Generate a string representation of the wallet  */
+    std::string toString();
+
+private:
+};
