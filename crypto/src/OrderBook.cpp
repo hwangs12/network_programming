bdfca67e5c449bd9d7834b684515a0921727adb8
hwangs12 <sungjun94@gmail.com>
2023-09-13 22:15:45 -0400

    implementing crypto trading platform

---
+++ crypto/src/OrderBook.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include "OrderBook.h"
+
+int OrderBook::orderBook(std::string)
+{
+    return 5;
+};
---
+++ crypto/src/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include "OrderBookEntry.h"
+#include "MerkelMain.h"
+#include "CSVReader.h"
+#include "Wallet.h"
+
+int main()
+{
+    // MerkelMain app();
+    // app.init();
+    Wallet wallet;
+    wallet.insertCurrency("BTC", 10);
+    std::cout << "Wallet has BTC " << wallet.containsCurrency("BTC", 10) << std::endl;
+    std::cout << wallet.toString() << std::endl;
+}
