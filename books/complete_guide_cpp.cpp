335c0b07d55931548146dbc86044c50f0d15b018
hwangs12 <sungjun94@gmail.com>
2023-09-15 21:45:43 -0400

    learn about constants

--- books/complete_guide_cpp.cpp
+++ books/complete_guide_cpp.cpp
@@ -8,4 +8,14 @@
 
 // for 16 bit computers, int is thus equivalent to short, whereas for 32-bit computers int will be equivalent to long.
 
-//
+// char <= short <= int <= long
+
+// short comprises 2 bytes and long 4 bytes
+
+// dec and hex change value to decimal and hexadecimal numbers
+
+// constants: boolean constants, numerical, character, string constants
+
+// octal begins with 0 and hex begins with 0x
+
+// the ranking for decimal constants are int, long, unsigned long
