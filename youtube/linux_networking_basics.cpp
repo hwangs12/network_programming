4e44a205d2e60fc838e4ed62a9ece00156e2b1c6
hwangs12 <sungjun94@gmail.com>
2023-09-27 22:41:12 -0400

    basic layout of how the socket programming will be written

---
+++ youtube/linux_networking_basics.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    // Create a socket
+    // Bind the socket to a IP / port
+    // Mark the socket for listening in
+    // Accept a call
+    // Close the listening socket
+    // While receiving - display message, echo message
+    // Close socket
+
+    return 0;
+}
