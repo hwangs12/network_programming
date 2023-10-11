88c0cd7fded24f62ee0b9eeb8e7cec6ed4b2dd77
hwangs12 <sungjun94@gmail.com>
2023-10-10 22:06:57 -0400

    just learning cpp

--- books/complete_guide_cpp.cpp
+++ books/complete_guide_cpp.cpp
@@ -19,3 +19,24 @@
 // octal begins with 0 and hex begins with 0x
 
 // the ranking for decimal constants are int, long, unsigned long
+
+/* string constants */
+
+/* string constant is stored internally without the quotes but terminated with a null character, \0, represented by a byte with a numerical value of 0 -- that is, all the bits in this byte are set to 0. thus, a string occupies one byte more in memory than the number of chars it contains. an empty string */
+
+/* "0" compises two bytes, the first byte containing the code for the character zero 0 and the second byte the value 0. */
+
+/* the terminating null character \0 is an example of an escape sequence. escape sequences are described in the following section. */
+
+/* USING CONTROL AND SPECIAL CHARACTERS */
+
+/* Nongraphic characters can be expressed by means of escape sequences, for example \t, which represents a tab. */
+
+/* The effect of an escape sequence will depend on the device concerned. The sequence \t, for example, depends on the setting for the tab width, which defaults to eight blanks but can be any value */
+
+int main()
+{
+    /* DEFINING VARIABLES */
+
+    /* A variable must be defined before you can use it in a program. When you define a variable the type is specified and an appropriate amount of memory reserved. This memory space is addressed by reference to the name of the variable. A simple definition has the following syntax: */
+}
