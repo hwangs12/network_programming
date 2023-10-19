14aadf5a2e04937cae5e66a7802785a4bc1a7917
hwangs12 <sungjun94@gmail.com>
2023-10-18 22:36:19 -0400

    opening and writing to a file

---
+++ udemy/intermediate/textfiles/writing.cpp
@@ -0,0 +1,29 @@
+/* WRITING A FILE USING FILE STREAM */
+#include <iostream>
+#include <fstream>
+
+using std::cerr;
+using std::cout;
+using std::endl;
+using std::fstream;
+using std::ofstream;
+
+int main()
+{
+    std::string filename = "yoyoma.txt";
+
+    ofstream filestream;
+    /* 1. open it. fstream::app option allows you to write line without overriding the file */
+    filestream.open(filename, fstream::app);
+    /* 2. if it's open write on it */
+    if (filestream.is_open())
+    {
+        filestream << "was it written?" << endl;
+        filestream << "Yes!" << endl;
+    }
+    /* 3. if not, notify */
+    else
+    {
+        cerr << "oops the file was not open" << endl;
+    }
+}
