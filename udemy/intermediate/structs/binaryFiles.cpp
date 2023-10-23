0b9de8c1b7dc5a981964a21e02288acc03639f66
hwangs12 <sungjun94@gmail.com>
2023-10-22 22:20:16 -0400

    writing then reading from that binary file

--- udemy/intermediate/structs/binaryFiles.cpp
+++ udemy/intermediate/structs/binaryFiles.cpp
@@ -30,5 +30,21 @@ int main()
         cout << "Could not create file " + fileName;
     }
 
+    /* READ BINARY FILE */
+    Person someoneElse = {};
+    ifstream inputFile;
+    inputFile.open(fileName, ios::binary);
+    if (inputFile.is_open())
+    {
+        inputFile.read(reinterpret_cast<char *>(&someoneElse), sizeof(Person));
+        inputFile.close();
+    }
+    else
+    {
+        cout << "Coud not read file " + fileName;
+    }
+
+    cout << someoneElse.name << ", " << someoneElse.age << ", " << someoneElse.weight << endl;
+
     return 0;
 }
