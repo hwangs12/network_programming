642b70620d2e73fb303e5ad7c94b817846a2284a
hwangs12 <sungjun94@gmail.com>
2023-10-29 21:19:05 -0400

    understanding lists

---
+++ learning/9_lists.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <list>
+using namespace std;
+
+int main()
+{
+    list<int> numbers;
+
+    numbers.push_back(1);
+    numbers.push_back(2);
+    numbers.push_back(3);
+    numbers.push_front(0);
+    numbers.insert(++numbers.begin(), 100);
+    numbers.erase(++numbers.begin());
+
+    for (list<int>::iterator it = numbers.begin(); it != numbers.end();)
+    {
+        if (*it == 2)
+        {
+            numbers.insert(it, 1234);
+        }
+
+        if (*it == 1)
+        {
+            it = numbers.erase(it);
+        }
+        else
+        {
+            it++;
+        }
+    }
+
+    for (list<int>::iterator it = numbers.begin(); it != numbers.end(); it++)
+    {
+        cout << *it << endl;
+    }
+
+    return 0;
+}
