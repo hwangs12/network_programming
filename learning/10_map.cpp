d7fbd1b48250174d40347dce17df4ceb04cee3f6
hwangs12 <sungjun94@gmail.com>
2023-10-31 22:17:23 -0400

    understanding key in a map

--- learning/10_map.cpp
+++ learning/10_map.cpp
@@ -13,7 +13,28 @@ int main()
     /* you can override these values */
     ages["Mike"] = 70;
 
+    ages.insert(pair<string, int>("Peter", 100));
+
     cout << ages["Rajesh"] << endl;
+
+    /* simply accessing non existent property will add to the map. Check the iteration below */
+    cout << ages["Sue"] << endl;
+
+    if (ages.find("tate") != ages.end())
+    {
+        cout << "Found Vicky" << endl;
+    }
+    else
+    {
+        cout << "Key not found" << endl;
+    }
+
+    for (map<string, int>::iterator it = ages.begin(); it != ages.end(); it++)
+    {
+        pair<string, int> age = *it;
+
+        cout << age.first << ": " << age.second << endl;
+    }
 
     for (map<string, int>::iterator it = ages.begin(); it != ages.end(); it++)
     {
