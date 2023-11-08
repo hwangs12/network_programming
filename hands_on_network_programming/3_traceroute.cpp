45c71e8057a6237116aa5ab173a557d39a6f3a0e
hwangs12 <sungjun94@gmail.com>
2023-11-07 22:18:16 -0500

    trace route

---
+++ hands_on_network_programming/3_traceroute.cpp
@@ -0,0 +1,17 @@
+/* Although this is not cpp executable, the idea is worth sharing */
+
+/* There are billions of routers with ip */
+
+/* when we try to reach dns, it asks for AAAA-type or A-type record */
+
+/* Once you receive you can connect to that server */
+
+/* There are billion routers to choose from how do request/response work so wonderfully? */
+
+/* How do we know which routes were taken for us to reach the destination server? */
+
+/* one way to find out is using traceroute */
+
+/*
+ Traceroute tells us which ip it walked through
+ */
