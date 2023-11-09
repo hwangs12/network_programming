465a76844d4800e3abbcef544891099a7ef4b91d
hwangs12 <sungjun94@gmail.com>
2023-11-08 21:39:49 -0500

    understanding adapter and address

---
+++ hands_on_network_programming/4_ifconfig.cpp
@@ -0,0 +1,14 @@
+/* IF YOU RUN IFCONFIG IN TERMINAL */
+
+/* YOU GET */
+
+/*
+ * lo0 = loopback
+ * gif0 = Software Network Interface
+ * stf0 = 6to4 tunnel interface
+ * en0 = Ethernet 0
+ * fw0 = Firewire
+ * en1 = Ethernet 1
+ * vmnet8 = Virtual Interface
+ * vmnet1 = Virtual Interface
+ */
