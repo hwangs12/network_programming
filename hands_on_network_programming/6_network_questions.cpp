b29272d8a65da6504f64e24a504d12e893597eb8
hwangs12 <sungjun94@gmail.com>
2023-11-09 20:40:01 -0500

    basic networking questions

---
+++ hands_on_network_programming/6_network_questions.cpp
@@ -0,0 +1,14 @@
+/* What are the key differences between IPv4 and IPv6 */
+/* A: */
+/* Are the IP addresses given by the ipconfig and ifconfig commands the same IP addresses that a remote web server sees if you connect to it? */
+/* A: No */
+/* What is the IPv4 loopback address? */
+/* A: 127.0.0.1 */
+/* What is the IPv6 loopback address? */
+/* A: ::1 */
+/* How are domain names resolved into IP addresses? */
+/* A: */
+/* How can you find your public IP address? */
+/* A: */
+/* How does an operating system know which application is responsible for an incoming packet? */
+/* A: */
