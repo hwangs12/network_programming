071003623547275de3d5404208ffbe6489713444
hwangs12 <sungjun94@gmail.com>
2023-11-11 21:41:43 -0500

    review of IP

--- hands_on_network_programming/6_network_questions.cpp
+++ hands_on_network_programming/6_network_questions.cpp
@@ -1,5 +1,5 @@
 /* What are the key differences between IPv4 and IPv6 */
-/* A: */
+/* A: IPv4 is 32 bits IPv6 is 128 bits. */
 /* Are the IP addresses given by the ipconfig and ifconfig commands the same IP addresses that a remote web server sees if you connect to it? */
 /* A: No */
 /* What is the IPv4 loopback address? */
@@ -7,8 +7,8 @@
 /* What is the IPv6 loopback address? */
 /* A: ::1 */
 /* How are domain names resolved into IP addresses? */
-/* A: */
+/* A: our computer sends UDP to DNS with message asking for AAAA records and A records to the DNS. */
 /* How can you find your public IP address? */
-/* A: */
+/* A: http://api.ipify.org */
 /* How does an operating system know which application is responsible for an incoming packet? */
-/* A: */
+/* A: through ports that incoming packet indicates */
