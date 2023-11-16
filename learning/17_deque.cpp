47446dc8fb3036908a39e1e71df2ec1a926401aa
hwangs12 <sungjun94@gmail.com>
2023-11-15 21:01:32 -0500

    deque starter

---
+++ learning/17_deque.cpp
@@ -0,0 +1,7 @@
+/* deque is an acronym of double-ended queue. */
+
+/* deques are sequence containers with dynamic sizes that can be expanded or contracted on both ends */
+
+/* Specific libraries may implement deques in different ways, generally as some form of dynamic array. */
+
+#include <deque>
