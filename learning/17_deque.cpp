8612d07e482447e566a5dd7f1418c97ce8b71d01
hwangs12 <sungjun94@gmail.com>
2023-11-16 19:50:08 -0500

    deque

--- learning/17_deque.cpp
+++ learning/17_deque.cpp
@@ -3,5 +3,22 @@
 /* deques are sequence containers with dynamic sizes that can be expanded or contracted on both ends */
 
 /* Specific libraries may implement deques in different ways, generally as some form of dynamic array. */
-
+#include <iostream>
 #include <deque>
+
+using std::cout;
+using std::deque;
+using std::endl;
+
+int main()
+{
+    deque<int> mapleSyrup;
+
+    mapleSyrup.push_back(5);
+    mapleSyrup.push_front(1);
+
+    for (int i : mapleSyrup)
+    {
+        cout << i << endl;
+    }
+}
