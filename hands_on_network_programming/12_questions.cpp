41c86626a28250c423e808e7181dc0e925590799
hwangs12 <sungjun94@gmail.com>
2023-11-18 21:26:07 -0500

    questions to address

---
+++ hands_on_network_programming/12_questions.cpp
@@ -0,0 +1,25 @@
+/**
+ * QUESTIONS
+ *
+ * 1. What is a socket?
+ *
+ * 2. What is a connectionless protocol?
+ *
+ * 3. What is a connection-oriented protocol?
+ *
+ * 4. Is TCP a connectionless or connection-oriented protocol?
+ *
+ * 5. What types of applications generally benefit from using the UDP protocol?
+ *
+ * 6. What types of applications benefit from using the TCP protocol?
+ *
+ * 7. Does TCP guarantee that data will be transmitted successfully?
+ *
+ * 8. What does the bind() function do?
+ *
+ * 9. What does the accept() function do?
+ *
+ * 10. In a TCP connection, does the client or the server send application data first?
+ *
+ *
+ */
