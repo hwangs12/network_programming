3462ed261a552483a1c2bb0b0376dd159260153e
hwangs12 <sungjun94@gmail.com>
2023-11-21 20:24:42 -0500

    answers

--- hands_on_network_programming/12_questions.cpp
+++ hands_on_network_programming/12_questions.cpp
@@ -11,8 +11,10 @@
  * A: If you need reliable communication, you may be tempted to develop a scheme wher eyou number each packet that's sent. For the first packet sent, you number it one, the second packet sent is numbered two, and so on. You could also request tat the receiver send an acknolwedgment for each packet. When the receiver gets packet one, it sends a return message, packet one received. In this way, the receiver can be sure that received packets are in the proper order.
  *
  * 4. Is TCP a connectionless or connection-oriented protocol?
+ * A: Connection-oriented protocol
  *
  * 5. What types of applications generally benefit from using the UDP protocol?
+ * A: Resolving DNS and Video streaming
  *
  * 6. What types of applications benefit from using the TCP protocol?
  *
