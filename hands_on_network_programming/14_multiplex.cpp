46a3eb24cf8c6fe2fee27f7cfde7067ef2b2ae27
hwangs12 <sungjun94@gmail.com>
2023-11-21 20:39:42 -0500

    premise

---
+++ hands_on_network_programming/14_multiplex.cpp
@@ -0,0 +1,14 @@
+/**
+ * socket APIs are blocking by default
+ * accept() blocks program until a new incoming connection is actually available.
+ * recv() blocks program until new data is actually available.
+ *
+ * imagine that a server needed to serve multiple client.
+ * then imagine that one slow client connected to it.
+ *
+ * the slow client takes a munute before sending its first data.
+ *
+ * During this time, our server would simply be waiting on the recv() call to return.
+ *
+ * If other clients were trying to connect, they would have to wait it out.
+ */
