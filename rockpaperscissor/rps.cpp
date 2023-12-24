f6ce884f5cffb9cf204866a4a882f240dedd1db1
hwangs12 <sungjun94@gmail.com>
2023-12-23 22:14:00 -0500

    hello

--- rockpaperscissor/rps.cpp
+++ rockpaperscissor/rps.cpp
@@ -8,8 +8,15 @@ int main()
     std::cout << "Hello there. Let's start the game. " << std::endl;
     std::cout << "This is rock paper scissor game." << std::endl;
     std::cout << "You are only allowed the following: ROCK, PAPER, SCISSOR" << std::endl;
+    std::cout << "Do you want to continue? Press 1 to start the game Press 0 to exit" << std::endl;
 
     std::cin >> user_input;
+
+    do 
+    {
+
+    }
+    while ()
 
     if (user_input != "ROCK" || user_input != "PAPER" || user_input != "SCISSOR")
     {
